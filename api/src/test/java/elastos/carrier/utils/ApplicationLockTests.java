05e32a1d0fc4d776a0762c6a3b7cdc958e89791e
Jingyu <jingyu.niu@gmail.com>
2023-03-29 14:56:45 +0800

    Add a native compatible application lock for launcher and shell

---
+++ api/src/test/java/elastos/carrier/utils/ApplicationLockTests.java
@@ -0,0 +1,35 @@
+package elastos.carrier.utils;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.junit.jupiter.api.Test;
+
+public class ApplicationLockTests {
+	private static final String tmpDir = System.getProperty("java.io.tmpdir");
+
+	@Test
+	void testLock() {
+		String lockFile = tmpDir + "/carrier.lock";
+
+		try (ApplicationLock lock = new ApplicationLock(lockFile)) {
+			System.out.println("Acquired lock!");
+
+			File f = new File(lockFile);
+			assertTrue(f.exists());
+			assertTrue(f.isFile());
+		} catch (IOException | IllegalStateException e) {
+			System.out.println("Acquired lock failed!");
+			fail("Acquired lock failed!", e);
+			return;
+		}
+
+		File f = new File(lockFile);
+		assertFalse(f.exists());
+		assertFalse(f.isFile());
+	}
+}
